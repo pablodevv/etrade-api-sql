8a8521145377a20e81b1bedeeff8dcd46153a4fa
Pablo Eduardo <pagamentos.pablorodriguez@gmail.com>
2025-02-20 15:48:35 -0300

    atualização env

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 const dbConfig = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    server: process.env.DB_SERVER,
+    server: '6.tcp.ngrok.io',
     database: process.env.DB_DATABASE,
     port: parseInt(process.env.DB_PORT, 10),
     options: {
