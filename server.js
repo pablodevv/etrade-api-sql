0e0f74d5316dc9559e0ff7a18e0bbb7b9bf546f1
Pablo Eduardo <pagamentos.pablorodriguez@gmail.com>
2025-02-20 16:49:45 -0300

    atualização api multiplas tabelas

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const port = process.env.PORT || 8100;
 app.use(cors());
 app.use(express.json());
 
-
 const dbConfig = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
@@ -23,16 +22,35 @@ const dbConfig = {
 };
 
 
-app.get("/dados", async (req, res) => {
+app.get("/tabelas", async (req, res) => {
     try {
         await sql.connect(dbConfig);
-        const result = await sql.query("SELECT * FROM Produto");
-        res.json(result.recordset);
+        const result = await sql.query(`
+            SELECT TABLE_NAME
+            FROM INFORMATION_SCHEMA.TABLES
+            WHERE TABLE_TYPE = 'BASE TABLE'
+        `);
+        const tabelas = result.recordset.map(item => item.TABLE_NAME);
+        res.json(tabelas);
     } catch (err) {
         res.status(500).send(err.message);
     }
 });
 
+
+app.get("/dados/:tabela/:coluna", async (req, res) => {
+    const { tabela, coluna } = req.params;
+
+    try {
+        await sql.connect(dbConfig);
+
+        const query = `SELECT ${coluna} FROM ${tabela}`;
+        const result = await sql.query(query);
+        res.json(result.recordset);
+    } catch (err) {
+        res.status(500).send(`Erro ao consultar dados: ${err.message}`);
+    }
+});
 
 app.listen(port, () => {
     console.log(`API rodando na porta ${port}`);
