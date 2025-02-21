a742135129d11f279c3fd402cee434799f2396d5
Pablo Eduardo <pagamentos.pablorodriguez@gmail.com>
2025-02-21 14:06:08 -0300

    Melhorias na API 2.0

--- server.js
+++ server.js
@@ -29,18 +29,18 @@ async function getAllTablesData() {
         await sql.connect(dbConfig);
 
         // Pega todas as tabelas do banco de dados
-        const tablesQuery =
+        const tablesQuery = `
             SELECT TABLE_NAME
             FROM INFORMATION_SCHEMA.TABLES
             WHERE TABLE_TYPE = 'BASE TABLE'
-        ;
+        `;
         const tablesResult = await sql.query(tablesQuery);
         const tables = tablesResult.recordset.map(item => item.TABLE_NAME);
 
         // Para cada tabela, pega os dados e retorna
         const data = {};
         for (const table of tables) {
-            const dataQuery = SELECT * FROM ${table};
+            const dataQuery = `SELECT * FROM ${table}`;
             const tableData = await sql.query(dataQuery);
             data[table] = tableData.recordset; // Adiciona os dados da tabela no objeto 'data'
         }
@@ -58,7 +58,7 @@ app.get("/dados", async (req, res) => {
         const allData = await getAllTablesData();
         res.json(allData); // Retorna os dados de todas as tabelas
     } catch (err) {
-        res.status(500).send(Erro: ${err.message});
+        res.status(500).send(`Erro: ${err.message}`);
     }
 });
 
@@ -70,21 +70,21 @@ app.get("/dados/juntar/:tabela1/:tabela2/:campo", async (req, res) => {
         await sql.connect(dbConfig);
 
         // Consulta para juntar duas tabelas
-        const query =
+        const query = `
             SELECT t1.*, t2.*
             FROM ${tabela1} AS t1
             JOIN ${tabela2} AS t2
             ON t1.${campo} = t2.${campo}
-        ;
+        `;
         const result = await sql.query(query);
 
         res.json(result.recordset); // Retorna os dados das duas tabelas unidas
     } catch (err) {
-        res.status(500).send(Erro: ${err.message});
+        res.status(500).send(`Erro: ${err.message}`);
     }
 });
 
 // Inicia o servidor
 app.listen(port, () => {
-    console.log(API rodando na porta ${port});
+    console.log(`API rodando na porta ${port}`);
 });
